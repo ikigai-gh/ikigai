6ed9fc3b8ec2f21f48e5052970d43049622db39b
ikigai <ikigai@ikigai.fail>
2022-11-09 23:13:37 +0300

    Add initial installation script

---
+++ src/install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+if ! which git >/dev/null 2>&1; then
+    echo "error: git is required, but was not found"
+    exit 1
+fi
+
+DOTFILES_REPO="https://github.com/ikigai-gh/dotfiles.git"
+echo "Konnichiwa, ${USER}! This script just clones a repo with dotfiles of ikigai user."
+git clone "${DOTFILES_REPO}"
